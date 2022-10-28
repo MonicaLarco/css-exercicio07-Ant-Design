8e1af320b4f3eee10096eb28ab12e8363fcea937
mlarco <mlarco94@gmail.com>
2022-10-28 01:37:26 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,120 @@
+import './App.css'
+import {
+  DesktopOutlined,
+  FileOutlined,
+  PieChartOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import { Breadcrumb, Col, Layout, Menu, Row, Card, Typography, Button, Input, Space, Progress, Spin, DatePicker, Slider } from 'antd';
+import React, { useState } from 'react';
+const { Header, Content, Footer, Sider } = Layout;
+function getItem(label, key, icon, children) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  };
+}
+const items = [
+  getItem('Option 1', '1', <PieChartOutlined />),
+  getItem('Option 2', '2', <DesktopOutlined />),
+  getItem('User', 'sub1', <UserOutlined />, [
+    getItem('Tom', '3'),
+    getItem('Bill', '4'),
+    getItem('Alex', '5'),
+  ]),
+  getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+  getItem('Files', '9', <FileOutlined />),
+];
+let { Title } = Typography;
+const App = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  return (
+    <Layout
+      style={{
+        minHeight: '100vh',
+      }}
+    >
+      <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+        <div className="logo" />
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+      </Sider>
+      <Layout className="site-layout">
+        <Header
+          className="site-layout-background"
+          style={{
+            padding: 0,
+          }}
+        />
+        <Content
+          style={{
+            margin: '0 16px',
+          }}
+        >
+          <Breadcrumb
+            style={{
+              margin: '16px 0',
+            }}
+          >
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div
+            className="site-layout-background"
+            style={{
+              padding: 24,
+              minHeight: 360,
+            }}
+          >
+            {/* <Title>Dashboard</Title> */}
+            <Row>
+              <Col>
+                <Card
+                  title="Default size card"
+                  extra={<a href="#">More</a>}
+                  style={{
+                    width: 300,
+                  }}
+                >
+                  <p>Card content</p>
+                  <p>Card content</p>
+                  <p>Card content</p>
+                  <Space direction='vertical'>
+                    <Button type='primary'>Button</Button>
+                    <Input placeholder='Type here...'></Input>
+                    <Progress percent={50} type='circle'></Progress>
+                    <Spin spinning></Spin>
+                    <DatePicker></DatePicker>
+                    <Slider cl></Slider>
+                  </Space>
+                </Card>
+                <Card
+                  size="small"
+                  title="Small size card"
+                  extra={<a href="#">More</a>}
+                  style={{
+                    width: 300,
+                  }}
+                >
+                  <p>Card content</p>
+                  <p>Card content</p>
+                  <p>Card content</p>
+                </Card>
+              </Col>
+            </Row>
+          </div>
+        </Content>
+        <Footer
+          style={{
+            textAlign: 'center',
+          }}
+        >
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
+  );
+};
+export default App;
